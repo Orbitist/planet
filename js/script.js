32b80f13013082846f8ea870f32e56e7d0ae2bd8
Nicholas Gunner <gunnernr@gmail.com>
2016-01-23 13:33:27 -0500

    Added better styles

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 var map = L.map('map').setView([37, -119], 10);
 
-L.tileLayer('https://tiles{0-3}.planet.com/v0/mosaics/open_california_re_20140101_20141231/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
+L.tileLayer('https://tiles2.planet.com/v0/mosaics/landsat8_toa_rgb_mosaic/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://planet.com">Planet Labs</a>',
     id: 'your.mapbox.project.id',
     accessToken: 'your.mapbox.public.access.token'
 }).addTo(map);
