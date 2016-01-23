140ee80716ea2bc619cd45237083495999aad60e
Nicholas Gunner <gunnernr@gmail.com>
2016-01-23 16:30:21 -0500

    working with map box code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
-var map = L.map('map').setView([37, -119], 10);
+L.mapbox.accessToken = 'pk.eyJ1Ijoib3JiaXRpc3QiLCJhIjoiYnpUTnJBdyJ9.uxgaJ0R9ZNsCcEnPNfo2ag';
+var map = L.mapbox.map('map').setView([37,-119], 6);
+var layers = document.getElementById('menu-ui');
 
-L.tileLayer('https://tiles2.planet.com/v0/mosaics/landsat8_toa_rgb_mosaic/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://planet.com">Planet Labs</a>',
-    id: 'your.mapbox.project.id',
-    accessToken: 'your.mapbox.public.access.token'
-}).addTo(map);
+addLayer(L.mapbox.tileLayer('mapbox.streets'), 'Base Map', 1);
+addLayer(L.tileLayer('https://tiles3.planet.com/v0/mosaics/landsat8_toa_rgb_mosaic/{z}/{x}/{y}.png'), 'Land Sat 8', 2);
+addLayer(L.tileLayer('https://tiles3.planet.com/v0/mosaics/color_balance_mosaic/{z}/{x}/{y}.png'), 'Color Balanced', 3);
+
+
+function addLayer(layer, name, zIndex) {
+    layer
+        .setZIndex(zIndex)
+        .addTo(map);
+
+    // Create a simple layer switcher that
+    // toggles layers on and off.
+    var link = document.createElement('a');
+        link.href = '#';
+        link.className = 'active';
+        link.innerHTML = name;
+
+    link.onclick = function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        if (map.hasLayer(layer)) {
+            map.removeLayer(layer);
+            this.className = '';
+        } else {
+            map.addLayer(layer);
+            this.className = 'active';
+        }
+    };
+
+    layers.appendChild(link);
+}
