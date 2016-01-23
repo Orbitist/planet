e45a01bbd58560814530d09b87c11b8ee679c95c
Nicholas Gunner <ngunner@Gs-MacBook-Pro.local>
2016-01-23 13:16:14 -0500

    added planet labs tiles

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([37, -119], 10);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+L.tileLayer('https://tiles{0-3}.planet.com/v0/mosaics/open_california_re_20140101_20141231/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'your.mapbox.project.id',
